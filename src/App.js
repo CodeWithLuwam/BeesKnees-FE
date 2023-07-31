44d1da08b1133aa905ef52c281572952826bc773
Luwam Ghebremicael <luwammikael@gmail.com>
2023-07-31 00:51:35 -0700

    displays information from database.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import { useState } from "react";
 
-function App() {
+
+// const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+const App = () => {
+  const [ exerciseName, setExerciseName ] = useState("Name")
+  const [ exerciseDescription, setExerciseDescription ] = useState("Description")
+  // const [ exerciseImage, setExerciseImage ] = useState("https://unsplash.com/photos/a-view-of-a-city-skyline-at-dusk-Z3bUl4t-tl0")
+  const getOneExercise =() => {
+    axios
+    .get("https://beesknees-be.onrender.com/exercises/")
+    .then((response) => {
+      // Code that executes with a successful response goes here
+      // console.log(response.data[0])
+      const exerciseData = response.data[0]
+      console.log(exerciseData.name)
+      setExerciseName(exerciseData.name)
+      setExerciseDescription(exerciseData.description)
+      // setExerciseImage(exerciseData.image)
+    })
+    
+    .catch((error) => {
+      // Code that executes with an unsuccessful response goes here
+    });    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={getOneExercise}>get One Exercise</button>
+    <h1>{exerciseName}</h1>
+    <h2>{exerciseDescription}</h2>
+    {/* <img src='https://unsplash.com/photos/a-view-of-a-city-skyline-at-dusk-Z3bUl4t-tl0' alt="img" /> */}
+    </>
   );
 }
 
