0322a80b6b4ea952b64b39d17843e2fb2583fa26
Luwam Ghebremicael <luwammikael@gmail.com>
2023-08-08 12:47:21 -0700

    moves axios call to App.js to access exercise data in exerciseList and tracker.

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,37 +1,36 @@
-import axios from "axios";
 import { useState , useEffect} from "react";
 import "../style/ExercisesList.css"
 import { Link } from "react-router-dom";
 import NaviBar from "./NaviBar";
 
 
-const ExercisesList = ( {baseURL}) => {
+const ExercisesList = ( {exercisesData, getAllExercises}) => {
   // const location = useLocation();
   // console.log(location)
-  const [exercisesData, setExercisesData] = useState([])
+  // const [exercisesData, setExercisesData] = useState([])
 
 
-  const getAllExercises =() => {
-    axios
-    .get(`${baseURL}exercises/`)
-    .then((response) => {
-      // Code that executes with a successful response goes here
-      // console.log(response)
-      const newData=response.data.map((exercise)=>{
-        return {
-          id:exercise.id,
-          name:exercise.name,
-          description:exercise.description,
-          image: exercise.image
-        }
-      })
-      setExercisesData(newData)
-      // console.log(exercisesData)
-    })
-    .catch((error) => {
-      // Code that executes with an unsuccessful response goes here
-    });    
-  }
+  // const getAllExercises =() => {
+  //   axios
+  //   .get(`${baseURL}exercises/`)
+  //   .then((response) => {
+  //     // Code that executes with a successful response goes here
+  //     // console.log(response)
+  //     const newData=response.data.map((exercise)=>{
+  //       return {
+  //         id:exercise.id,
+  //         name:exercise.name,
+  //         description:exercise.description,
+  //         image: exercise.image
+  //       }
+  //     })
+  //     setExercisesData(newData)
+  //     // console.log(exercisesData)
+  //   })
+  //   .catch((error) => {
+  //     // Code that executes with an unsuccessful response goes here
+  //   });    
+  // }
 
   useEffect(()=>getAllExercises(),[])
   const displayExercises = () => {
