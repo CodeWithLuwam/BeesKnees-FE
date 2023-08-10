f77943c51e814ff05e577265e007e1a41ac4384b
Luwam Ghebremicael <luwammikael@gmail.com>
2023-08-09 23:14:34 -0700

    displays entries for specific user

--- src/components/History.js
+++ src/components/History.js
@@ -8,6 +8,7 @@ import Table from "react-bootstrap/Table";
 const History = ({ baseURL, email }) => {
   const [userData, setUserData] = useState([]);
   const [exerciseData, setExerciseData] = useState([]);
+  const [currentUser, setCurrentUser] = useState("");
 
   const getExerciseData = () => {
     axios
@@ -24,13 +25,20 @@ const History = ({ baseURL, email }) => {
     axios
       .get(`${baseURL}user/`)
       .then((response) => {
-        // console.log(response);
         const newUserData = response.data.map((user) => ({
           id: user.id,
           name: user.name,
+          email: user.email,
           entries: user.entries,
         }));
         setUserData(newUserData);
+      })
+      .then(() => {
+        userData.forEach((newUser) => {
+          if (email === newUser.email) {
+            setCurrentUser(newUser.name);
+          }
+        });
       })
 
       .catch((error) => {
@@ -48,34 +56,36 @@ const History = ({ baseURL, email }) => {
     exerciseMap[exercise.id] = exercise.name;
   });
 
-  const displayUserEntries = () => {
-    return userData.map((user, index) => (
-      <React.Fragment key={index}>
-        <tr>
-          <th>Exercise</th>
-          <th>Date</th>
-          <th>Sets</th>
-          <th>Reps/Mins</th>
-        </tr>
-        {user.entries.map((entry) => (
-          <tr key={entry.id}>
-            <td className="input-text">{exerciseMap[entry.exercise]}</td>
-            <td className="input-text">{entry.date}</td>
-            <td className="input-text">{entry.sets}</td>
-            <td className="input-text">{entry.reps_or_mins}</td>
-          </tr>
-        ))}
-      </React.Fragment>
-    ));
-  };
-
   return (
     <>
       <NaviBar />
       <div>
-        <div id="title">Your History</div>
+        <div id="title"> {currentUser} History</div>
         <Table className="traker-table" striped bordered hover variant="light">
-          <tbody>{displayUserEntries()}</tbody>
+          <thead>
+            <tr>
+              <th>Exercise</th>
+              <th>Date</th>
+              <th>Sets</th>
+              <th>Reps/Mins</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userData
+              .filter((user) => email === user.email)
+              .map((user) =>
+                user.entries.map((entry) => (
+                  <tr key={entry.id}>
+                    <td className="input-text">
+                      {exerciseMap[entry.exercise]}
+                    </td>
+                    <td className="input-text">{entry.date}</td>
+                    <td className="input-text">{entry.sets}</td>
+                    <td className="input-text">{entry.reps_or_mins}</td>
+                  </tr>
+                ))
+              )}
+          </tbody>
         </Table>
       </div>
     </>
